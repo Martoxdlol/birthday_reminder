cf02513ccccd68abfc12ec814c417892f3607ea4
IanConsulting <martoxdlol@gmail.com>
2023-06-22 22:53:39 -0300

    enable handling app links

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,13 @@
 import 'package:birthday_reminder/auth_wrapper.dart';
 import 'package:birthday_reminder/firebase_options.dart';
-import 'package:birthday_reminder/js_bindings.dart';
+import 'package:birthday_reminder/js_bindings/js_ignore_bindings.dart'
+    if (dart.library.html) 'package:birthday_reminder/js_bindings/js_bindings.dart';
 import 'package:birthday_reminder/util.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:app_links/app_links.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -23,12 +25,35 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  late AppLinks _appLinks;
+
   @override
   void initState() {
+    if (!kIsWeb) {
+      initDeepLinks();
+    }
     if (kIsWeb) {
       appFinishedLoading();
     }
     super.initState();
+  }
+
+  Future<void> initDeepLinks() async {
+    _appLinks = AppLinks();
+
+    // Check initial link if app was in cold state (terminated)
+    final appLink = await _appLinks.getInitialAppLink();
+    if (appLink != null) {
+      navigateToUri(appLink);
+    }
+
+    _appLinks.uriLinkStream.listen(navigateToUri);
+  }
+
+  void navigateToUri(Uri uri) {
+    final params = uri.queryParameters;
+    final segments = uri.pathSegments;
+    // TODO: Implement shared link handling
   }
 
   @override
